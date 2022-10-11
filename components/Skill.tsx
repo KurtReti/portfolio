809324110e2d6c7c268625ac4eeeb652ba766c5c
KurtReti <kurt.reti@gmail.com>
2022-10-11 13:32:13 +1100

    skill fix

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -17,6 +17,10 @@ export default function Skill({skill, proficiency,color}) {
         whileHover={{
             scale:1.2
         }}
+        whileTap={{
+          scale:1.2
+        }}
+        
 
       className={` flex grayscale brightness-200 hover:brightness-100 hover:grayscale-0 transition-colors flex-col items-center justify-center opacity-60 group hover:opacity-100  text-white hover:shadow-xl hover:shadow-zinc-900 duration-500  h-24 rounded-full   w-${24}`}>    
         <DevIcon icon={skill} style={devIconStyle} viewBox="0 0 32 32"/>
